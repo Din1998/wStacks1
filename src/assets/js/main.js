60709f3b8577bb9b92979e3aeaac8b6e85e39283
https://github.com/Din1998 <dinislam8803@gmail.com>
2023-06-11 17:44:23 +0600

    add feature section,card,ripple btn

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -601,8 +601,11 @@ window.onload = function(){
   }
 
   VanillaTilt.init(document.querySelectorAll(".card_body"), {
-		max: 15,
-		speed: 15,
+		max: 2,
+		speed: 4,
+    glare: true,
+    "max-glare": 0.4,
+    
     
 	});
 
@@ -614,34 +617,6 @@ window.addEventListener("scroll",function(){
   var topNav = document.querySelector(".header-area");
   topNav.classList.toggle("header-sticky", window.scrollY > 280);
 });
-
-// ripple
-function createRipple(event) {
-  const button = event.currentTarget;
-
-  const circle = document.createElement("span");
-  const diameter = Math.max(button.clientWidth, button.clientHeight);
-  const radius = diameter / 2;
-
-  circle.style.width = circle.style.height = `${diameter}px`;
-  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
-  circle.classList.add("ripple");
-
-  const ripple = button.getElementsByClassName("ripple")[0];
-
-  if (ripple) {
-    ripple.remove();
-  }
-
-  button.appendChild(circle);
-}
-
-const buttons = document.getElementsByTagName("button");
-for (const button of buttons) {
-  button.addEventListener("click", createRipple);
-}
-
 
 
 
